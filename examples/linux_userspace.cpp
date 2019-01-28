c2e331a208c1a7a56372e1f08e809da82203079d
flagarde <lagarde@sjtu.edu.cn>
2019-01-28 21:21:14 +0800

    Test with two sensors

--- examples/linux_userspace.cpp
+++ examples/linux_userspace.cpp
@@ -24,30 +24,30 @@ int8_t stream_sensor_data_forced_mode(bme280& bm)
   rslt = bm.bme280_set_sensor_settings(settings_sel);
   printf("Temperature, Pressure, Humidity\r\n");
   /* Continuously stream sensor data */
-  while (1) 
-  {
-    rslt = bm.bme280_set_sensor_mode(BME280_FORCED_MODE);
+rslt = bm.bme280_set_sensor_mode(BME280_FORCED_MODE);
     /* Wait for the measurement to complete and print data @25Hz */
-    bm.delay_ms(40);
+bm.delay_ms(40);
     rslt = bm.bme280_get_sensor_data(BME280_ALL);
     print_sensor_data(bm.getData());
-  }
   return rslt;
 }
 
 int main(int argc, char* argv[])
 {
   I2C i2c(argv[1],"0x76");
+  I2C i2c2(argv[1],"0x77");
   settings setting;
   setting.setOversamplingPressure("16X");
   setting.setOversamplingHumidity("1X");
   setting.setOversamplingTemperature("2X");
   setting.setFilterCoefficient("16");
   i2c.connect();
+  i2c2.connect();
   bme280 bm(i2c,setting);
-  bm.m_IO=&i2c;
+  bme280 bm2(i2c2,setting);
   int8_t rslt = BME280_OK;
-
   rslt = bm.bme280_init();
+  rslt = bm2.bme280_init();
   stream_sensor_data_forced_mode(bm);
+  stream_sensor_data_forced_mode(bm2);
 }
